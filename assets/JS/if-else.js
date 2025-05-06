8ebb17592cbc1470c17c776204f97374200519bc
Pablo Vargas <Rojaspablotomas@gmail.com>
2025-05-06 16:49:21 -0500

    Se añade if-else.js

---
+++ assets/JS/if-else.js
@@ -0,0 +1,24 @@
+let a = 5;
+
+if ( a >= 10 ) {
+    console.log("a es mayor o igual que 10");
+} else {
+    console.log("a es menor que 10");
+}
+
+console.log("Fin de programa");
+
+const hoy = new Date(); 
+let dia = hoy.getDay(); 
+
+console.log({dia});
+
+if ( dia === 0 ) {
+    console.log("Domingo");
+} else if (dia === 1) {
+    console.log("Lunes");
+} else if (dia === 2) {
+    console.log("Martes");
+} else { console.log("No es lunes, martes o domingo...");
+}
+
