6490151f3c1144bddba1bc393aea410629d7f24e
Pablo Vargas <Rojaspablotomas@gmail.com>
2025-05-07 09:35:47 -0500

    Se agrega Logica-Booleana

---
+++ assets/JS/logica-booleana.js
@@ -0,0 +1,28 @@
+const regresaTrue = () => {
+    console.log("Regresa true");
+    return true;
+}
+
+const regresaFalse = () => {
+    console.log("Regresa False");
+    return false;
+}
+
+console.warn("Not o la negacion");
+console.log(true); // true
+console.log(!true); // false  
+console.log(!false); // true   
+
+console.log(!regresaFalse()); // TRUE
+
+console.warn("And"); //TRUE SI TODOS LOS VALORES SON VERDADEROS
+console.log(true && true); // true
+console.log(true && false); // false   
+
+regresaFalse() && regresaTrue();
+
+console.warn("OR");
+
+console.log(true || false); // true 
+console.log(false || false); // false
+console.log(regresaTrue() || regresaFalse()); // true
