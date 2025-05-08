0f6f1156b40ea60bb6a62ea4b2585e76830ca4f6
Pablo Vargas <Rojaspablotomas@gmail.com>
2025-05-08 12:32:28 -0500

    Se agrega el archivo switch

---
+++ assets/JS/switch.js
@@ -0,0 +1,27 @@
+const dia = 1; // 0 = domingo
+
+switch (dia) {
+    case 0:
+        console.log("Es domingo");
+        break;
+    case 1:
+        console.log("Es lunes");
+        break;
+    case 2:
+        console.log("Es martes");
+        break;
+    case 3:
+        console.log("Es miercoles");
+        break;
+    case 4:
+        console.log("Es jueves");
+        break;
+    case 5:
+        console.log("Es viernes");
+        break;
+    case 6:
+        console.log("Es sabado");
+        break;
+    default:
+        console.log("No es un dia de la semana")
+}
